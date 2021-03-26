6392ae1d0a9cf7e2416977487316fbd810725855
Иван Васильев <vanya.vasil21@mail.ru>
2021-03-26 22:14:55 +0300

    commit

---
+++ NEW-Version/web/js.js
@@ -0,0 +1,26 @@
+function showModalWin() {
+    let darkLayer = document.createElement('div'); // слой затемнения
+    darkLayer.id = 'shadow'; // id чтобы подхватить стиль
+    document.body.appendChild(darkLayer); // включаем затемнение
+    let modalWin = document.getElementById('popupWin'); // находим наше "окно"
+    modalWin.style.display = 'block'; // "включаем" его
+    darkLayer.onclick = function () { // при клике на слой затемнения все исчезнет
+        darkLayer.parentNode.removeChild(darkLayer); // удаляем затемнение
+        modalWin.style.display = 'none'; // делаем окно невидимым
+        return false;
+    };
+}
+
+function Complete()
+{
+    if(document.FormShadow.Pass1.value==document.FormShadow.Pass2.value )
+    {
+        alert("Пароль правильный");
+    }
+    else
+    {
+        document.FormShadow.Pass1.style.borderColor = "red";
+        document.FormShadow.Pass2.style.borderColor = "red";
+        alert("Ошибка ввода пароля");
+    }
+}
