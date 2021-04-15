e268cacfeb57e010aaa2d77e31ad2b1ce36b9b28
Иван Васильев <vanya.vasil21@mail.ru>
2021-04-15 11:26:41 +0300

    l

---
+++ NEW-Version/Lab6/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <?php
+    include("Header.php");
+    include("menu.php");
+    include("content.php");
+    include("footer.php");
+    ?>
+</body>
+</html>
